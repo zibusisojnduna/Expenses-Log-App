284837218c66176eb75dff1e29ca5b738bd2182f
User <User@WRSDLD309-040aa.dut.ac.za>
2024-08-19 14:22:09 +0200

    updates

--- src/components/displayTransaction.jsx
+++ src/components/displayTransaction.jsx
@@ -8,8 +8,11 @@ function DisplayTransaction(props){
                 <div className="list-item">
                     <div><h6>{data.transactionItem}</h6></div>
                     <div><h6>{data.amount}</h6></div>
-                    {data.transactionType == "Expense" ? <div className="expenseIndicator"></div>: <div className="incomeIndicator"></div>}
-                    <div><h6>{data.transactionType}</h6></div>
+                    
+                    <div>{data.transactionType == "Expense" ? <div className="expenseIndicator"></div>: <div className="incomeIndicator"></div>}
+                    <h6>{data.transactionType}</h6>
+                    </div>
+                    
                     <div/>
                     
                 </div>
